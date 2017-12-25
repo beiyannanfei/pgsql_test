f5f204e2270655e54596de942835f728bf5a7f55
wangyanqiang <wangyanqiang@sensoro.com>
2017-12-25 18:14:53 +0800

    update

--- app/01_connection.js
+++ app/01_connection.js
@@ -11,7 +11,45 @@ const pool = new Pool({
 	port: 5432,
 });
 
-pool.query('SELECT NOW()', (err, res) => {
-	console.log(err);
-	console.log(res);
+function t1() {
+	pool.query('SELECT NOW()', (err, res) => {
+		console.log(err);
+		console.log("%j", res.rows);  //[{"now":"2017-12-25T10:10:54.296Z"}]
+	});
+}
+
+function t2() {
+	pool.query("select * from weather", (err, res) => {
+		console.log(err);
+		console.log("%j", res.rows);
+		let rows = [
+			{"city": "San Francisco", "temp_lo": 46, "temp_hi": 50, "prcp": 0.25, "date": "1994-11-26T16:00:00.000Z"},
+			{"city": "San Francisco", "temp_lo": 43, "temp_hi": 57, "prcp": 0, "date": "1994-11-28T16:00:00.000Z"},
+			{"city": "Hayward", "temp_lo": 37, "temp_hi": 54, "prcp": null, "date": "1994-11-28T16:00:00.000Z"}
+		];
+	});
+}
+
+const client = new Client({
+	user: 'wyq',
+	host: '127.0.0.1',
+	database: 'mydb',
+	password: '',
+	port: 5432,
 });
+client.connect();
+
+function t3() {
+	client.query("select * from weather").then(response => {
+		console.log("rows = %j", response.rows);
+		rows = [
+			{"city": "San Francisco", "temp_lo": 46, "temp_hi": 50, "prcp": 0.25, "date": "1994-11-26T16:00:00.000Z"},
+			{"city": "San Francisco", "temp_lo": 43, "temp_hi": 57, "prcp": 0, "date": "1994-11-28T16:00:00.000Z"},
+			{"city": "Hayward", "temp_lo": 37, "temp_hi": 54, "prcp": null, "date": "1994-11-28T16:00:00.000Z"}
+		];
+	}).catch(err => {
+		console.log("err: %j", err.message || err);
+	});
+}
+
+
---
+++ app/02_connStr.js
@@ -0,0 +1,7 @@
+/**
+ * Created by wyq on 17/12/25.
+ * 连接URI
+ */
+
+
+
