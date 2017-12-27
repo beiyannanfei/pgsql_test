0ce38ce23e0e73e8b9be29bfbd613c93e3d4f772
wangyanqiang <wangyanqiang@sensoro.com>
2017-12-27 17:26:16 +0800

    update

---
+++ app/07_poolApi.js
@@ -0,0 +1,43 @@
+/**
+ * Created by wyq on 17/12/27.
+ */
+const {Pool} = require('pg');
+setTimeout(process.exit, 1000, 0);
+
+const pool = new Pool({
+	user: 'wyq',
+	host: '127.0.0.1',
+	database: 'mydb',
+	password: '',
+	port: 5432,
+	max: 20,
+	idleTimeoutMillis: 30000,
+	connectionTimeoutMillis: 2000,    //0 为无限期挂起
+});
+
+//从池中获取客户端。如果池已满并且所有客户端当前都已检出，则将等待一个FIFO队列，直到客户端被释放回池为止。
+// 如果池中有空闲的客户端，它将返回到回调process.nextTick。如果池未满，将创建新客户端并将其返回给此回调。
+pool.connect((err, client, release) => {
+	if (err) {
+		return console.error('Error acquiring client', err.stack);
+	}
+	client.query('SELECT NOW()', (err, result) => {
+		release();    //是否连接
+		if (err) {
+			return console.error('Error executing query', err.stack);
+		}
+		console.log(result.rows);
+	})
+});
+
+/*
+const { Pool } = require('pg')
+
+const pool = new Pool()
+
+;(async function(){
+  const client = await pool.connect()
+  await client.query('SELECT NOW()')
+  client.release()//如果您忘记释放客户端，那么您的应用程序将迅速耗尽池中闲置的可用客户端，并且所有进一步的调用pool.connect将会由于错误而超时，或者如果connectionTimeoutMills配置为0，则无限期挂起。
+})()
+ */
--- app/pool.js
+++ app/pool.js
@@ -8,5 +8,13 @@ const pool = new Pool({
 	database: 'mydb',
 	password: '',
 	port: 5432,
+	max: 20,    //maximum number of clients the pool should contain.by default this is set to 10.
+	// number of milliseconds a client must sit idle in the pool and not be checked out
+	// before it is disconnected from the backend and discarded
+	// default is 10000 (10 seconds) - set to 0 to disable auto-disconnection of idle clients
+	idleTimeoutMillis: 30000,
+	// number of milliseconds to wait before timing out when connecting a new client
+	// by default this is 0 which means no timeout
+	connectionTimeoutMillis: 2000,
 });
 module.exports = pool;
