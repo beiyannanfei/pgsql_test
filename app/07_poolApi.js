aa8c4688b94e3fa4740b00896453eceaf5e4166e
wangyanqiang <wangyanqiang@sensoro.com>
2017-12-27 17:30:01 +0800

    update

--- app/07_poolApi.js
+++ app/07_poolApi.js
@@ -41,3 +41,26 @@ const pool = new Pool()
   client.release()//如果您忘记释放客户端，那么您的应用程序将迅速耗尽池中闲置的可用客户端，并且所有进一步的调用pool.connect将会由于错误而超时，或者如果connectionTimeoutMills配置为0，则无限期挂起。
 })()
  */
+
+/*
+const { Pool } = require('pg')
+const pool = new Pool()
+//通常，我们只需要在数据库上运行一个查询，为了方便，池有一个方法来在第一个可用的空闲客户端上运行查询并返回结果。
+pool.query('SELECT $1::text as name', ['brianc'], (err, result) => {
+  if (err) {
+    return console.error('Error executing query', err.stack)
+  }
+  console.log(result.rows[0].name) // brianc
+})
+
+ */
+
+/*
+const { Pool } = require('pg')      //pool.query（）=> Promise
+const pool = new Pool()
+
+pool.query('SELECT $1::text as name', ['brianc'])
+  .then((res) => console.log(res.rows[0].name)) // brianc
+  .catch(err => console.error('Error executing query', err.stack))
+ */
+
