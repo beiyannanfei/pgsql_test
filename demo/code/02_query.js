45766dda5ae3696a6236a80c59b00d3a9a3e43e5
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-02 16:35:20 +0800

    update

--- demo/code/02_query.js
+++ demo/code/02_query.js
@@ -75,8 +75,33 @@ function t3(score) {
 
 // t3(`90;drop table users;`); //sql注入
 
+function t4(score) {
+	co(function* () {
+		let delRes = yield client.query("drop table if exists users");
+		let createTbRes = yield client.query(
+			`CREATE TABLE users (
+			name varchar(80),
+			subject varchar(80),
+			score int)`
+		);
+		let insRes = yield client.query(`insert into users values
+			('zs', 'js', 85),
+			('zs', 'python', 92),
+			('ls', 'c++', 50),
+			('ls', 'jave', 78),
+			('ww', 'go', 98),
+			('ww', 'php', 84)
+		`);
+		let text = `select * from users where score > $1`;
+		let values = [score];
+		let {rows} = yield client.query(text, values);
+		console.log("rows = %j", rows);
+	}).catch(err => {
+		console.log(err.message || err);
+	});
+}
 
-
-
+// t4(84); //rows = [{"name":"zs","subject":"js","score":85},{"name":"zs","subject":"python","score":92},{"name":"ww","subject":"go","score":98}]
+// t4(`90;drop table users;`); //invalid input syntax for integer: "90;drop table users;"
 
 
