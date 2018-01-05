f4590e673b43d050ea3255c6b328674a51c1ca98
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-05 18:36:02 +0800

    update

--- demo/code/07_cursor-api.js
+++ demo/code/07_cursor-api.js
@@ -1,11 +1,79 @@
 /**
  * Created by wyq on 18/1/4.
  */
-const types = require('pg').types;
+const {Pool} = require("pg");
+const Cursor = require('pg-cursor');
+const co = require("co");
+setTimeout(process.exit, 1000, 0);
 
-types.setTypeParser(20, function (val) {
-	return parseInt(val)
-});
+const pool = new Pool({connectionString: 'postgresql://wyq:@127.0.0.1:5432/demo_db'});
+
+function t1() {
+	let cursorSync = function (cursor, count = 10) {
+		return new Promise((resolve, reject) => {
+			cursor.read(count, (err, rows) => {
+				if (!!err) {
+					return reject(err);
+				}
+				return resolve(rows);
+			});
+		});
+	};
+	co(function* () {
+		const client = yield pool.connect();
+		yield client.query("drop table if exists t1");
+		yield client.query(`CREATE TABLE t1 (num int)`);
+		console.log("begin insert data");
+		for (let i = 0; i < 1000; ++i) {
+			yield client.query(`insert into t1 values(${i})`);
+		}
+		console.log("insert 1000 item success");
+		let cursor = client.query(new Cursor("select * from t1"));
+		// console.log(cursor);
+		/*cursor.read(10, (err, rows) => {  //注意：cursor没有promise模式
+			if (!!err) {
+				console.log("err: %j", err.stack || err.message || err);
+				throw err;
+			}
+			console.log("rows = %j", rows);
+			cursor.read(5, (err, rows) => {
+				console.log("rows = %j", rows);
+			});
+		});*/
+		cursor.readSync = function (count = 10) {
+			let self = this;
+			return new Promise((resolve, reject) => {
+				self.read(count, (err, rows) => {
+					if (!!err) {
+						return reject(err);
+					}
+					return resolve(rows);
+				});
+			});
+		};
+		while (rows = yield cursor.readSync()) {
+
+		}
+		/*let rows = yield cursor.readSync();
+		console.log("rows = %j", rows);
+		rows = yield  cursor.readSync(5);
+		console.log("rows = %j", rows);*/
+	}).catch(err => {
+		console.log(err.stack || err.message || err);
+	});
+}
+
+t1();
+
+
+
+
+
+
+
+
+
+
 
 
 
